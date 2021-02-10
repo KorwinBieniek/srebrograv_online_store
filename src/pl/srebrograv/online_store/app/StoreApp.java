d13581727c7d6b6d439078c1da297212a67f4c97
KorwinBieniek <korwin-bieniek@wp.pl>
2021-02-10 21:02:09 +0100

    Added String to keep version of the store application. Initialized controller object in the app class.

--- src/pl/srebrograv/online_store/app/StoreApp.java
+++ src/pl/srebrograv/online_store/app/StoreApp.java
@@ -1,7 +1,11 @@
 package pl.srebrograv.online_store.app;
 
 public class StoreApp {
+    private static final String APP_NAME = "Srebrograv v1.0";
+
     public static void main(String[] args) {
-        System.out.println("Hello store!");
+        System.out.println(APP_NAME);
+        StoreControl storeControl = new StoreControl();
+
     }
 }
