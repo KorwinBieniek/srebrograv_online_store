321063f5dd5ed135fed5ba252ca69458e61443f1
KorwinBieniek <korwin-bieniek@wp.pl>
2021-02-16 16:33:55 +0100

    Added methods to create items and create customers for the store.

--- src/pl/srebrograv/online_store/io/DataReader.java
+++ src/pl/srebrograv/online_store/io/DataReader.java
@@ -1,4 +1,45 @@
 package pl.srebrograv.online_store.io;
 
+import pl.srebrograv.online_store.model.Bracelet;
+import pl.srebrograv.online_store.model.StoreCustomer;
+import pl.srebrograv.online_store.model.Xeranthemum;
+
+import java.util.Scanner;
+
 public class DataReader {
+    private Scanner sc = new Scanner(System.in);
+    private ConsolePrinter printer;
+
+    public Xeranthemum readAndCreateXeranthemum() {
+        printer.printLine("Pendant name: ");
+        String pendantName = sc.nextLine();
+        printer.printLine("Metal type: ");
+        String metalType = sc.nextLine();
+        printer.printLine("Engravement message: ");
+        String engravementMessage = sc.nextLine();
+
+        return new Xeranthemum(pendantName, metalType, engravementMessage);
+    }
+
+    public Bracelet readAndCreateBracelet() {
+        printer.printLine("Pendant name: ");
+        String pendantName = sc.nextLine();
+        printer.printLine("Metal type: ");
+        String metalType = sc.nextLine();
+        printer.printLine("Strap type: ");
+        String StrapType = sc.nextLine();
+
+        return new Bracelet(pendantName, metalType, StrapType);
+    }
+
+    public StoreCustomer createStoreCustomer() {
+        printer.printLine("Name");
+        String firstName = sc.nextLine();
+        printer.printLine("Last name");
+        String lastName = sc.nextLine();
+        printer.printLine("Pesel");
+        String pesel = sc.nextLine();
+        return new StoreCustomer(firstName, lastName, pesel);
+    }
+
 }
