9eb1d2ccb9dc51c88d1d246736e6beda47a29087
KorwinBieniek <korwin-bieniek@wp.pl>
2021-02-26 08:54:51 +0100

    Final app release

--- src/pl/srebrograv/online_store/app/StoreApp.java
+++ src/pl/srebrograv/online_store/app/StoreApp.java
@@ -1,7 +1,7 @@
 package pl.srebrograv.online_store.app;
 
 public class StoreApp {
-    private static final String APP_NAME = "Srebrograv v1.5";
+    private static final String APP_NAME = "Srebrograv v2.0";
 
     public static void main(String[] args) {
         System.out.println(APP_NAME);
